59f0ddf259052ce8385a25a21af4f7ecc174f687
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-01-19 02:21:59 +0000

    Complete prep-javascript-for-loops.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,15 @@
+var i = '';
+
+for (i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+var i = '';
+
+for (i = 0; i < 10; i++) {
+  console.log(i * 2);
+}
+
+for (i = 100; i > 0; i--) {
+  console.log('Time till explosion: ', i);
+}
